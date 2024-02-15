874b3f3884ef1ba4c0bfa96d1333cb24934dc311
thefourth24 <pgmnievera.student@ua.edu.ph>
2024-02-15 12:06:29 +0800

    first commit

---
+++ pages/index.js
@@ -0,0 +1,41 @@
+import Head from 'next/head';
+import Layout, { siteTitle } from '../components/layout';
+import utilStyles from '../styles/utils.module.css';
+
+export default function Home() {
+  return (
+    <Layout home>
+      <Head>
+        <title>{siteTitle}</title>
+      </Head>
+      <section className={utilStyles.headingMd}>
+        <p> Lou Guerrig Mendoza My Hobbies are Playing, Powerlifting, my favorite music is Remember the name. my favorite food is
+Pizza.</p>
+  <ul>
+  <li>My expection on this subject is to gain more knowledge about system intergration and architecture</li>
+  <li>I want to learn the basic foundation of this subject</li>
+  <li>I want be familiar with deploying website and gain more understanding about it</li>
+  </ul>
+  
+     <p>10 Things That Require Zero Talent</p>
+        <ul>
+        <li>1. Being on Time</li>
+        <li>2. Making An Effort</li>
+        <li>3. Being High Energy</li>
+        <li>4. Having A Positive Attitude</li>
+        <li>5. Being Passionate</li>
+        <li>6. Using Good Body Language</li>
+        <li>7. Being Coachable</li>
+        <li>8. Doing A Little Extra</li>
+        <li>9. Being Prepared</li>
+        <li>10. Having A Strong Work Ethic</li>
+        </ul>
+
+        <p>
+          (This is a sample website - you’ll be building a site like this on{' '}
+          <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
+        </p>
+      </section>
+    </Layout>
+  );
+}
